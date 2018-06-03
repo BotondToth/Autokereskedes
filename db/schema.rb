45a980c9e381588c380926dad5ac919739a51405
BotondToth <tothboti@gmail.com>
2018-06-03 22:21:03 +0200

    Semaphoere build fail fix #2

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2018_06_03_053952) do
     t.integer "cc"
     t.integer "mileage"
     t.string "description"
-    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -4,4 +4,9 @@ class CarTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+	test "should save car" do
+	  car = Car.new
+	  assert car.save
+	end
 end
